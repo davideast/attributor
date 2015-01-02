6c9551b52bf72846e932a6d675a216e72f0ec2a1
David East <david@firebase.com>
2015-01-02 07:04:00 -0800

    TextStatsViewController

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by deast on 1/2/15.
+//  Copyright (c) 2015 Firebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
